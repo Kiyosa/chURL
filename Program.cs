d66575800d637818ba9ab612d023d29d57a8c023
dex black <kiyo.sa@dexblack.net>
2024-02-16 15:39:47 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace chURL
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing command line args. /? for usage.");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}

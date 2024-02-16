df5c32079ac3cd9287969dd3f933119dede8aa5c
dex black <kiyo.sa@dexblack.net>
2024-02-16 16:22:08 +1100

    Console and File logging implemented

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.Console;
 
 namespace chURL
 {
@@ -12,7 +14,48 @@ namespace chURL
                 return 1;
             }
 
+            // Create a StreamWriter to write logs to a text file
+            StreamWriter logFileWriter = new(args[0], append: false);
+
+            ILogger logger = ConfigureLogger(logFileWriter);
+            // Output some text on the console
+            using (logger.BeginScope("[scope is enabled]"))
+            {
+                logger.LogInformation("Hello World!");
+                logger.LogInformation("Logs contain timestamp and log level.");
+                logger.LogInformation("Each log message is fit in a single line.");
+
+                logger.LogInformation("Info Log");
+                logger.LogWarning("Warning Log");
+                logger.LogError("Error Log");
+                logger.LogCritical("Critical Log");
+            }
+
             return 0;
         }
+
+        static ILogger ConfigureLogger(StreamWriter logFileWriter)
+        {
+            // Create an ILoggerFactory
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                //Add console output
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+                });
+
+                // Add a custom log provider to write logs to text files
+                builder.AddProvider(new CustomFileLoggerProvider(logFileWriter));
+            });
+
+            // Create an ILogger
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+            return logger;
+        }
+
     }
 }

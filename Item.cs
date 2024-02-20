60c80fba07fddd79a53c32f3735bd6f1fe198f33
dex black <kiyo.sa@dexblack.net>
2024-02-20 12:54:14 +1100

    DateTime fidelity b0rken, using string. ToString format corrected for round trip test.

--- Item.cs
+++ Item.cs
@@ -9,21 +9,21 @@ namespace chURL
     internal class Item()
     {
         public string id { get; set; } = string.Empty;
-        public DateTime createdDate { get; set; }
+        public string createdDate { get; set; } = string.Empty;
         public Dictionary<string, string> properties { get; set; } = new Dictionary<string, string>();
         public string title { get; set; } = string.Empty;
 
         override public string ToString()
         {
-            string d = createdDate.ToString("yyyy-MM-ddTHH:mm:ss");
-            string s = $"{{ \"id\": \"{id}\", \"createdDate\": \"{d}\", \"title\": \"{title}\", ";
+            // string d = createdDate.ToString("yyyy-MM-ddTHH:mm:ss."); // DateTime loses fidelity of decimal seconds
+            string s = $"{{ \"id\": \"{id}\", \"createdDate\": \"{createdDate}\", ";
             string p = "\"properties\": { ";
             foreach (KeyValuePair<string, string> kvp in properties)
             {
-                p += $"\"{kvp.Key}\": \"{kvp.Value}\",";
+                p += $"\"{kvp.Key}\": \"{kvp.Value}\", ";
             }
             if (p.Length > 0) {
-                s += p;
+                s += p.Remove(p.Length - 2);
             }
             s += $" }}, \"title\": \"{title}\" }}";
             return s;
